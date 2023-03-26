5f6abab3a6691e0f554815c055cefa39c6ddae73
dickyts15 <dickytegar15@gmail.com>
2023-03-27 06:07:45 +0700

    Bikin Header dan include ke tiap page

---
+++ layouts/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manajemen Tugas</title>
+
+    <!-- CSS Styles -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:white">
+    <a class="navbar-brand p-1 ms-3 mt-1" href="#">
+        <img src="/resources/Logo Manajemen Tugas.png" width="100" height="100" alt="Logo">
+    </a>
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <h3>Home</h3>
+                </a>
+            </li>
+            <li></li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <h3>About</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <h3>Menu</h3>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <h3>Contact Us</h3>
+                </a>
+            </li>
+        </ul>
+
+        <a class="btn px-4 me-5" href="login.php" style="color:white; background-color:#159895; mix-blend-mode:darken; box-shadow: 0px 4px 4px rgba(0,0,0,0.25);">LOGIN</a>
+    </div>
+</nav>
+
+<body>
+
+</body>
+
+</html>
---
+++ server/connection.php
@@ -0,0 +1,9 @@
+<?php
+    $conn = mysqli_connect(
+        "localhost", 
+        "root", 
+        "", 
+        "malefashion_app"
+        ) 
+        or die("Can't connect to the database");
+?>
