c4804cb85e943fa629f56a5f37379adc4020b6dc
Nazillah Wahidyah <nazillahwahidiyah@gmail.com>
2023-04-06 21:23:15 +0700

    modified index.php dan tambah_tugas.php

--- index.php
+++ index.php
@@ -14,7 +14,26 @@
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <p>Home Page sa</p>
+    <main>
+        <div>
+            <div class="container">
+                <div class="hero">
+                    <div class="content">
+                        <div class="left-content">
+                        <h1 style="font-weight: bold">Welcome To Our<br>Manajemen Tugas</h1>
+                        <p>Manajemen Tugas membantu pelajar<br>
+                        dalam mengatur tugas - tugas yang<br>
+                        harus diprioritaskan terlebih dahulu<br>
+                        agar tidak melewati tenggat waktu</p>
+                        </div>
+                        <div class="right-content">
+                            <img src="image/check-homepage.png" width="566px" height="463px" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 
--- tambah_tugas.php
+++ tambah_tugas.php
@@ -1,5 +1,5 @@
 <?php
-    include('layouts/headerNotSigned.php');
+    include('layouts/headerSigned.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +10,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manajemen Tugas</title>
 </head>
-<body>
+<body style="background-color: #EA5455">
+<section>
+<div class="left-content">
+<form>
+        <div class="main">
+            <h1 style="font-weight: bold; color: #ffffff; margin-left: 45px;
+            margin-top: 30px">Tambah Tugas</h1>
+            <div class="form-group">
+                <div>
+                    <label for="inputJudul">Judul</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div>
+                    <label for="inputDeskripsi">Deskripsi</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div>
+                    <label for="inputDate">Date</label>
+                    <input type="Date" class="form-control">
+                </div>
+            </div>
+            <div class="form-check-container">
+            <div class="form-check">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio1">To Do</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio2">Doing</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio">
+                <label class="form-check-label" for="inlineRadio3">Done</label>
+            </div>
+            </div>
+            <button class="btnTambah" type="submit">Tambah</button>
+        </div>
+    </form>
+</div>
+</section>
+
     
 </body>
 </html>
