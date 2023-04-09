42b73c9a47157e3fc31e2e63c2a6ef63440dcdc2
Nazillah Wahidyah <nazillahwahidiyah@gmail.com>
2023-04-09 12:48:55 +0700

    login register

--- login.php
+++ login.php
@@ -1,5 +1,39 @@
 <?php
-    include('layouts/headerNotSigned.php');
+    session_start();
+    include('server/connection.php');
+
+    if (isset($_SESSION['logged_in'])){
+        header('location: dashboard.php');
+        exit;
+    }
+
+    if (isset($_POST['submit'])){
+        $email = stripslashes($_POST['email']);
+        $email = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_POST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+
+        $query= "SELECT * FROM user WHERE username= '$username' OR email= '$email'";
+        $result = mysqli_query($con, $query);
+        $rows = mysqli_num_rows(result);
+
+
+        if($rows !=0){
+            $hash = mysqli_fetch_assoc($result)['password'];
+            if(password_verify($password, $hash)){
+                session_start();
+                $_SESSION["username"] = $username;
+
+                header("location: dashboard.php");
+            }
+        }
+    }
+
+?>
+
+
+<?php
+include('layouts/headerNotSigned.php');
 ?>
 
 <!DOCTYPE html>
--- register.php
+++ register.php
@@ -1,5 +1,35 @@
 <?php
-    include('layouts/header.php');
+    session_start();
+    include('server/connection.php');
+    
+    if(isset($_POST['submit'])){
+
+
+        $nama= stripslashes($_POST['name']);
+        $nama= mysqli_real_escape_string($con, $nama);
+        $username= mysqli_real_escape_string($con['username']);
+        $email= stripslashes($_POST['email']);
+        $email= mysqli_real_escape_string($con, $nama);
+        $password= stripslashes($_POST['password']);
+        $password= mysqli_real_escape_string($con, $password);
+
+        $sql= "INSERT INTO user (nama, username, email, password)
+            VALUES ('$nama', '$username', '$email' , '$password')";
+        $result= mysqli_query($con, $query);
+
+        if ($result){
+            $_SESSION['username'] = $username;
+
+            header('location: dashboard.php');
+        
+        } 
+    } 
+?>
+
+
+
+<?php
+include('layouts/headerNotSigned.php');
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +41,48 @@
     <title>Manajemen Tugas</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="card my-5">
+            <div class="row no-gutters">
+                <div class="col-6 py-6 rounded-start" align="center" style="background-color: #EA5455; color:white">
+
+                    <div class="card-img my-4">
+                        <img src="/image/Sign up-bro.png" width="400px" alt="">
+                    </div>
+                    <h1><b>
+                            Start for Free and
+                            <br> Manage your task Easily
+                        </b></h1>
+                </div>
+                <div class="col-6 py-6 rounded-end" style="background-color:white">
+                    <div class="container py-3 px-7">
+                        <h1 style="margin-bottom:-2px">Get's Started.</h1>
+                        <p class="text-nowrap text-muted">Already have an account? <a class="text-danger text-decoration-none text-opacity-75" href="login.php"><b>Log in</b></a></p>
+                        <form>
+                            <label for="nameForm"><b>
+                                    <h4>Nama</h4>
+                                </b></label>
+                            <input type="text" class="form-control form-control-lg mb-3 shadow-form rounded-0" style="border-left: 6px solid #159895" id="nameForm" placeholder="Nama">
+                            <label for="usernameForm"><b>
+                                    <h4>Username</h4>
+                                </b></label>
+                            <input type="text" class="form-control form-control-lg mb-3 shadow-form rounded-0" style="border-left: 6px solid #159895" id="usernameForm" placeholder="Username">
+
+                            <label for="emailForm"><b>
+                                    <h4>Email</h4>
+                                </b></label>
+                            <input type="email" class="form-control form-control-lg mb-3 shadow-form rounded-0" style="border-left: 6px solid #159895" id="emailForm" placeholder="Email">
+                            <label for="passwordForm"><b>
+                                    <h4>Password</h4>
+                                </b></label>
+                            <input type="password" class="form-control form-control-lg mb-3 shadow-form rounded-0" style="border-left: 6px solid #159895" id="passwordForm" placeholder="Password">
+                            <center><input class="btn btn-outline-light shadow my-4 px-5 py-2" style="background-color:#159895;" type="submit" value="Register"></center>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
